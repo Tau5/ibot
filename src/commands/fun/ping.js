3a44e1fb2b434b0de5a43536c16074225daa3aea
iDroid <biojoutk1106@gmail.com>
2017-09-20 14:49:00 +0200

    Fix some bugs

--- src/commands/fun/ping.js
+++ src/commands/fun/ping.js
@@ -1,5 +1,5 @@
 exports.execute = async (client, ctx) => {
-  ctx.channel.send('Ping...').then(m => m.edit(`Pong! Discord API: **${ctx.client.ping}** - Local: **${(m.createdTimestamp - ctx.createdTimestamp)}ms**`));
+  ctx.channel.send('Ping...').then(m => m.edit(`Pong! Discord API: **${client.ping}ms** - Local: **${(m.createdTimestamp - ctx.createdTimestamp)}ms**`));
 };
 
 exports.conf = {
--- src/commands/utility/about.js
+++ src/commands/utility/about.js
@@ -1,10 +1,10 @@
 exports.execute = async (client, ctx) => {
   const { MessageEmbed } = require('discord.js');
   const embed = new MessageEmbed()
-    .addField(client.I18n.translate`Owner`, 'iDroid#4441')
-    .addField(client.I18n.translate`Servers`, ctx.client.guilds.size)
-    .addField(client.I18n.translate`RAM Usage`, `${Math.round(process.memoryUsage().rss / 100000)}MB`)
-    .addField(client.I18n.translate`Support server`, 'https://discord.gg/invite')
+    .addField(client.I18n.translate`Owner`, 'iDroid#4441', true)
+    .addField(client.I18n.translate`Servers`, ctx.client.guilds.size, true)
+    .addField(client.I18n.translate`RAM Usage`, `${Math.round(process.memoryUsage().rss / 1000000)}MB`, true)
+    .addField(client.I18n.translate`Support server`, 'https://discord.gg/invite', true)
     .setColor(ctx.guild.me.displayHexColor)
     .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 1024 }));
 
