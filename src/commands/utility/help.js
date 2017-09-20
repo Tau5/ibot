a1c16533253683c23c96508f6696e14c352dec57
iDroid <biojoutk1106@gmail.com>
2017-09-20 21:15:42 +0200

    Ah!

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,5 +1,5 @@
 exports.execute = async (client, ctx) => {
-  ctx.channel.send(client.I18n.translate`📚 Commands list :\n\`\`\`${client.commands.filter(c => c.public).keyArray().join(', ')}.\`\`\``);
+  ctx.channel.send(client.I18n.translate`📚 Commands list :\n\`\`\`${client.commands.filter(c => c.conf.public).keyArray().join(', ')}.\`\`\``);
 };
 
 exports.conf = {
