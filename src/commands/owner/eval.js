001f9e8267e63ef20208e7092a5e187e1956804e
iDroid <biojoutk1106@gmail.com>
2017-09-21 20:21:28 +0200

    Eval enhancements

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 exports.execute = async (client, ctx) => {
+  const Discord = require('discord.js');
+  const fs = require('fs');
+
   try {
     const code = ctx.args.join(' ');
     let evaled = await eval(code); // eslint-disable-line no-eval
 
     if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
     evaled = evaled.replace(/client.token/g, 'TOKEN');
+
+    if (evaled.length > 1900) return ctx.channel.send('❌ The output length exceeds 1024 caracters!');
 
     return ctx.channel.send(evaled, { code: 'js' });
   } catch (e) {
