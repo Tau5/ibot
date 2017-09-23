27d4af275f434896fd1600f476906adf2444b94a
iDroid <biojoutk1106@gmail.com>
2017-09-23 14:49:23 +0200

    Enhancement

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -2,7 +2,9 @@ exports.execute = async (client, ctx) => {
   const command = client.commands.get(ctx.args.join(' ')) || client.commands.get(client.aliases.get(ctx.args.join(' ')));
   if (command) {
     if (!command.conf.public) return ctx.channel.send(client.I18n.translate`❌ Em wait a minute! You're not supposed to see that.`);
-    ctx.author.send(`📚 Help for \`${command.conf.name}\` :\n**Description:** ${client.help[command.conf.name].description}\n**Usage:** i:${client.help[command.conf.name].usage}`);
+    ctx.author.send(`📚 Help for \`${command.conf.name}\` :\n**Description:** ${client.help[command.conf.name].description}\n**Usage:** i:${client.help[command.conf.name].usage}`).then(() => {
+      ctx.channel.send(client.I18n.translate`✅ Sent in DM`);
+    }).catch(e => ctx.channel.send(e, { code: 'js' }));
   } else {
     ctx.channel.send(client.I18n.translate`📚 Commands list :\n\`\`\`${client.commands.filter(c => c.conf.public).keyArray().join(', ')}.\`\`\``);
   }
