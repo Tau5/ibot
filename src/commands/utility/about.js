a14dcfdebb0886f3997645765974e2c3f6a27724
iDroid <biojoutk1106@gmail.com>
2017-09-23 08:41:42 +0200

    Switching from rss to heapUsed

--- src/commands/utility/about.js
+++ src/commands/utility/about.js
@@ -3,7 +3,7 @@ exports.execute = async (client, ctx) => {
   const embed = new MessageEmbed()
     .addField(client.I18n.translate`Owner`, 'iDroid#4441', true)
     .addField(client.I18n.translate`Servers`, ctx.client.guilds.size, true)
-    .addField(client.I18n.translate`RAM Usage`, `${Math.round(process.memoryUsage().rss / 1000000)}MB`, true)
+    .addField(client.I18n.translate`RAM Usage`, `${Math.round(process.memoryUsage().heapUsed / 1000000)}MB`, true)
     .addField(client.I18n.translate`Support server`, 'https://discord.gg/invite', true)
     .addField(client.I18n.translate`Translators`, '🇫🇷 iDroid#4441 - 🇩🇪 TimNook#0323')
     .setColor(ctx.guild.me.displayHexColor)
