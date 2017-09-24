d8bd7bae3f6929ae4f31547fac6420a41e297776
Kevin B <biojoutk1106@gmail.com>
2017-09-24 09:01:56 +0200

    Switch:tm: to Apixu

--- src/commands/utility/weather.js
+++ src/commands/utility/weather.js
@@ -4,26 +4,23 @@ exports.execute = async (client, ctx) => {
   if (location.length > 1024) return ctx.channel.send(client.I18n.translate`❌ The city length may not exceed 1024 caracters.`);
 
   const request = require('request');
-  request(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${client.config.weather_api}&units=metric`, (err, http, body) => {
+  request(`https://api.apixu.com/v1/forecast.json?key=${client.config.weather_api}&q=${location}`, (err, http, body) => {
     if (err && http.statusCode !== 200) throw err;
 
     const content = JSON.parse(body);
-    if (content.cod !== 200) return ctx.channel.send(client.I18n.translate`❌ The specified city was not found!`);
-
-    const sunset = new Date(content.sys.sunset * 1000);
-    const sunrise = new Date(content.sys.sunrise * 1000);
+    if (content.error && content.error.code === 1006) return ctx.channel.send(client.I18n.translate`❌ The specified city was not found!`);
 
     const { MessageEmbed } = require('discord.js');
     const embed = new MessageEmbed()
-      .addField(client.I18n.translate`Weather`, `${content.weather[0].main}\n(${content.weather[0].description})`, true)
-      .addField(client.I18n.translate`Miscallenous`, client.I18n.translate`**Clouds:** ${content.clouds.all}%`, true)
-      .addField(client.I18n.translate`Temperatures`, client.I18n.translate`**Current:** ${content.main.temp}°C\n**Min:** ${content.main.temp_min}°C - **Max:** ${content.main.temp_max}°C`, true)
-      .addField(client.I18n.translate`Daytime (UTC)`, client.I18n.translate`**Sunrise:** ${sunrise.getUTCHours()}:${sunrise.getUTCMinutes()} - **Sunset:** ${sunset.getUTCHours()}:${sunset.getUTCMinutes()}`, true)
+      .addField(client.I18n.translate`Weather`, content.current.condition.text, true)
+      .addField(client.I18n.translate`Miscallenous`, client.I18n.translate`**Clouds:** ${content.current.cloud}%`, true)
+      .addField(client.I18n.translate`Temperatures`, client.I18n.translate`**Current:** ${content.current.temp_c}°C (${content.current.temp_f}°F)\n**Min:** ${content.forecast.forecastday[0].day.mintemp_c}°C (${content.forecast.forecastday[0].day.mintemp_f}°F) - **Max:** ${content.forecast.forecastday[0].day.maxtemp_c}°C (${content.forecast.forecastday[0].day.maxtemp_f}°F)`, true)
+      .addField(client.I18n.translate`Daytime (UTC)`, client.I18n.translate`**Sunrise:** ${content.forecast.forecastday[0].astro.sunrise} - **Sunset:** ${content.forecast.forecastday[0].astro.sunset}`, true)
       .setColor(ctx.guild.me.displayHexColor)
-      .setFooter(client.I18n.translate`All information provided by Open Weather Map`, 'https://upload.wikimedia.org/wikipedia/commons/1/15/OpenWeatherMap_logo.png')
-      .setThumbnail(`https://persoidroid.000webhostapp.com/img/${content.weather[0].icon}.png`);
+      .setFooter(client.I18n.translate`All information provided by Apixu`, 'https://pbs.twimg.com/profile_images/761439376433971200/ljkftqN4.jpg')
+      .setThumbnail(`https:${content.current.condition.icon}`);
 
-    return ctx.channel.send(client.I18n.translate`☀ Weather for **${content.name}** (${content.sys.country}) :`, { embed });
+    return ctx.channel.send(client.I18n.translate`☀ Weather for **${content.location.name}** (${content.location.region} - ${content.location.country}) :`, { embed });
   });
 };
 
