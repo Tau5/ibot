bfcee5bb2ec4f0d0b90bc732e83a37c2cb6b8894
Kevin B <biojoutk1106@gmail.com>
2017-09-24 09:06:17 +0200

    Fixed again stuff

--- src/commands/utility/weather.js
+++ src/commands/utility/weather.js
@@ -15,7 +15,7 @@ exports.execute = async (client, ctx) => {
       .addField(client.I18n.translate`Weather`, content.current.condition.text, true)
       .addField(client.I18n.translate`Miscallenous`, client.I18n.translate`**Clouds:** ${content.current.cloud}%`, true)
       .addField(client.I18n.translate`Temperatures`, client.I18n.translate`**Current:** ${content.current.temp_c}°C (${content.current.temp_f}°F)\n**Min:** ${content.forecast.forecastday[0].day.mintemp_c}°C (${content.forecast.forecastday[0].day.mintemp_f}°F) - **Max:** ${content.forecast.forecastday[0].day.maxtemp_c}°C (${content.forecast.forecastday[0].day.maxtemp_f}°F)`, true)
-      .addField(client.I18n.translate`Daytime (UTC)`, client.I18n.translate`**Sunrise:** ${content.forecast.forecastday[0].astro.sunrise} - **Sunset:** ${content.forecast.forecastday[0].astro.sunset}`, true)
+      .addField(client.I18n.translate`Daytime`, client.I18n.translate`**Sunrise:** ${content.forecast.forecastday[0].astro.sunrise} - **Sunset:** ${content.forecast.forecastday[0].astro.sunset}`, true)
       .setColor(ctx.guild.me.displayHexColor)
       .setFooter(client.I18n.translate`All information provided by Apixu`, 'https://pbs.twimg.com/profile_images/761439376433971200/ljkftqN4.jpg')
       .setThumbnail(`https:${content.current.condition.icon}`);
