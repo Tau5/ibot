13e3cdaeeca1a378e3d866825aa7cca82ac561e5
Kevin B <biojoutk1106@gmail.com>
2017-09-24 17:11:18 +0200

    Funny thing

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -9,7 +9,7 @@ exports.execute = async (client, ctx) => {
     if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
     evaled = evaled.replace(/client.config.token/g, 'TOKEN');
 
-    if (evaled.length > 1900) return ctx.channel.send('❌ The output length exceeds 1024 caracters!');
+    if (evaled.length > 1900) return ctx.channel.send('❌ The output length exceeds Kyle2000 caracters!');
 
     return ctx.channel.send(evaled, { code: 'js' });
   } catch (e) {
