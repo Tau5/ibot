e48deb272b547f85ccf4567d4e0ca84e46fb6fa2
Kevin B <biojoutk1106@gmail.com>
2017-09-28 19:08:31 +0200

    Fix ?

--- src/commands/fun/choose.js
+++ src/commands/fun/choose.js
@@ -1,8 +1,9 @@
 exports.execute = async (client, ctx) => {
   const choices = ctx.args;
   if (choices.length < 2) return ctx.channel.send(client.I18n.translate`❌ You must include two choices or more!`);
+  const chosen = choices[Math.random() * choices.length];
 
-  ctx.channel.send(client.I18n.translate`🤔 Let me choose... **${choices[Math.random() * choices.length]}**!`);
+  ctx.channel.send(client.I18n.translate`🤔 Let me choose... **${chosen}**!`);
 };
 
 exports.conf = {
