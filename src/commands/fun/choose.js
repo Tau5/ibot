cea21bfb03326b0e81f97504713d0728db6eb44c
Kevin B <biojoutk1106@gmail.com>
2017-09-28 19:10:08 +0200

    Let's disable it until I find a workaround

--- src/commands/fun/choose.js
+++ src/commands/fun/choose.js
@@ -1,9 +1,11 @@
 exports.execute = async (client, ctx) => {
-  const choices = ctx.args;
+  /* const choices = ctx.args;
   if (choices.length < 2) return ctx.channel.send(client.I18n.translate`❌ You must include two choices or more!`);
   const chosen = choices[Math.random() * choices.length];
 
-  ctx.channel.send(client.I18n.translate`🤔 Let me choose... **${chosen}**!`);
+  ctx.channel.send(client.I18n.translate`🤔 Let me choose... **${chosen}**!`); */
+
+  ctx.channel.send('❌ Command disabled due to some weird bugs... Sorry!');
 };
 
 exports.conf = {
