77fb0731b402a4af670d90298166e521a0d5022f
Kevin B <biojoutk1106@gmail.com>
2017-10-08 16:28:19 +0200

    Dashboard v1 (ready to publish)

---
+++ src/web/servers/index.js
@@ -0,0 +1,8 @@
+module.exports = (client) => {
+  const express = require('express');
+  const router = express.Router();
+
+  router.use('/', (req, res) => res.status(200).render('servers', { mutualGuilds: client.guilds.filter(g => g.members.has(req.user.id)), identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') }));
+
+  return router;
+};
