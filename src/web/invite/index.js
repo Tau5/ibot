84248d75a2b6dc0cf1cb67b4ce78c177d2f5dc7a
Kevin B <biojoutk1106@gmail.com>
2017-10-08 22:38:31 +0200

    Updated one thing

--- src/web/invite/index.js
+++ src/web/invite/index.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
   const router = express.Router();
 
   router.use('/:id', (req, res) => {
-    res.redirect(`https://discordapp.com/oauth2/authorize?&client_id=305277118105911296&scope=bot&guild_id=${req.params.id}&response_type=code&redirect_uri=http%3A%2F%2F78.202.166.128%2Fserver%2F`);
+    res.redirect(`https://discordapp.com/oauth2/authorize?&client_id=305277118105911296&scope=bot&guild_id=${req.params.id}&response_type=code&redirect_uri=http%3A%2F%2Fwww.ibot-discord.cf%3A9024%2Fserver`);
   });
 
   router.use('/', (req, res) => {
