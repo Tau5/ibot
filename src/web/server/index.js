7c05e3c89bb08ce0a3632d0179ca6869ed52441d
Kevin B <biojoutk1106@gmail.com>
2017-10-11 20:23:20 +0200

    Is it okay ?

--- src/web/server/index.js
+++ src/web/server/index.js
@@ -2,16 +2,6 @@ module.exports = (client) => {
   const express = require('express');
   const router = express.Router();
 
-  router.use('/:id', (req, res) => {
-    if (!client.guilds.has(req.params.id)) return res.status(404).render('error', { code: '404', identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') });
-    const guild = client.guilds.get(req.params.id);
-    const config = client.servers.get(req.params.id);
-
-    res.status(200).render('server', {
-      guild, config, user: req.user, identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO'),
-    });
-  });
-
   router.post('/update/:id', (req, res) => {
     if (!client.guilds.has(req.params.id)) return res.status(404).render('error', { code: '404', identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') });
     const guild = client.guilds.get(req.params.id);
@@ -35,6 +25,16 @@ module.exports = (client) => {
 
     client.servers.set(req.params.id, newConfig);
     res.redirect(`/server/${req.params.id}`);
+  });
+
+  router.use('/:id', (req, res) => {
+    if (!client.guilds.has(req.params.id)) return res.status(404).render('error', { code: '404', identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') });
+    const guild = client.guilds.get(req.params.id);
+    const config = client.servers.get(req.params.id);
+
+    res.status(200).render('server', {
+      guild, config, user: req.user, identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO'),
+    });
   });
 
   router.use('/', (req, res) => {
