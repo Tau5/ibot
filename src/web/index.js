58b5ef6473fe5033e8c3bc000061f68d561ef2aa
iDroid <biojoutk1106@gmail.com>
2017-10-14 14:14:32 +0200

    Security

--- src/web/index.js
+++ src/web/index.js
@@ -34,11 +34,13 @@ module.exports = (client) => {
   const updateSession = (req, res, next) => {
     const request = require('request');
     request('https://discordapp.com/api/users/@me', { headers: { Authorization: `Bearer ${req.cookies.accessToken}` } }, (err, http, body) => {
+      if (err) res.redirect('/auth/login');
       const user = body;
       user.provider = 'discord';
 
 
       request('https://discordapp.com/api/users/@me/guilds', { headers: { Authorization: `Bearer ${req.cookies.accessToken}` } }, (err2, http2, body2) => {
+        if (err) res.redirect('/auth/login');
         user.guilds = body2;
         req.session.passport = {
           user,
