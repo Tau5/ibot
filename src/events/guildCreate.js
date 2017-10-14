8354120fb5cb404b737050b91a72c28a6f2b7795
iDroid <biojoutk1106@gmail.com>
2017-10-14 14:28:36 +0200

    It was very annoying

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,7 +1,7 @@
 module.exports = async (client, guild) => {
   /* LOGGING */
   console.log(`[Servers] Join - ${guild.name} (ID:${guild.id}) - Check the guilds log for more informations.`);
-  require('fs').appendFile('./logs/guilds.txt', `[${require('moment-timezone')().tz('UTC').format('DD/MM/YYYY HH:mm:ss')}] JOIN - ${guild.name} (ID:${guild.id}) - Owner: ${guild.owner.user.tag} (ID:${guild.ownerID}) - Members: ${guild.memberCount} (${guild.members.filter(m => m.user.bot).size} bots) - Creation: ${guild.createdAt.toUTCString()}\r\n`, err => console.error(err));
+  require('fs').appendFile('./logs/guilds.txt', `[${require('moment-timezone')().tz('UTC').format('DD/MM/YYYY HH:mm:ss')}] JOIN - ${guild.name} (ID:${guild.id}) - Owner: ${guild.owner.user.tag} (ID:${guild.ownerID}) - Members: ${guild.memberCount} (${guild.members.filter(m => m.user.bot).size} bots) - Creation: ${guild.createdAt.toUTCString()}\r\n`, console.error);
 
   /* GAME */
   client.botUtil.updateGame(client);
