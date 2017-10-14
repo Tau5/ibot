684d037923e2a347023b83abbf71b9b2024bc1a4
iDroid <biojoutk1106@gmail.com>
2017-10-14 12:32:56 +0200

    It should work fine now

--- src/web/auth/auth.js
+++ src/web/auth/auth.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const utils = require('util');
 
+const users = new Enmap({ name: 'users', persistent: true });
+
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((id, done) => {
   done(null, id);
@@ -16,6 +18,12 @@ passport.use(new DiscordStrategy({
   callbackURL: config.callbackURL,
 }, (accessToken, refreshToken, profile, done) => {
   if (accessToken === null) return;
+  const user = {
+    accessToken,
+    refreshToken,
+    profile,
+  };
+  users.set(profile.id, user);
   done(null, profile);
 }));
 
