ffbe9f6abc775554540223eb007a5c859cf6c384
Kevin B <biojoutk1106@gmail.com>
2017-10-16 20:07:23 +0200

    That may be better

--- src/helpers/modUtil.js
+++ src/helpers/modUtil.js
@@ -1,26 +1,28 @@
 const modUtils = {
   Serverlog(client, guild, message, options) {
-    try {
-      const config = client.servers.get(guild.id);
-      if (config.switch_serverlog === 0) return;
-      const channel = guild.channels.get(config.channel_serverlog);
-      if (!channel) throw new Error('NO_SERVERLOG_CHANNEL');
+    const config = client.servers.get(guild.id);
+    if (config.switch_serverlog === 0) return;
+    const channel = guild.channels.get(config.channel_serverlog);
+    if (!channel) throw new Error('NO_SERVERLOG_CHANNEL');
 
-      const msg = `\`[${require('moment-timezone')().tz(config.timezone).format('HH:mm:ss')}]\` ${message}`;
-      channel.send(msg, options);
-    } catch (e) {}
+    let time = require('moment-timezone')().tz('UTC').format('HH:mm:ss');
+    try { time = require('moment-timezone')(config.timezone).tz('UTC').format('HH:mm:ss'); } catch (e) {}
+
+    const msg = `\`[${time}]\` ${message}`;
+    channel.send(msg, options);
   },
 
   Modlog(client, guild, message, reason, attachments = []) {
-    try {
-      const config = client.servers.get(guild.id);
-      if (config.switch_modlog === 0) return;
-      const channel = guild.channels.get(config.channel_modlog);
-      if (!channel) throw new Error('NO_MODLOG_CHANNEL');
+    const config = client.servers.get(guild.id);
+    if (config.switch_modlog === 0) return;
+    const channel = guild.channels.get(config.channel_modlog);
+    if (!channel) throw new Error('NO_MODLOG_CHANNEL');
 
-      const msg = client.I18n.translate`\`[${require('moment-timezone')().tz(config.timezone).format('HH:mm:ss')}]\` \`[${config.moderation.length}]\` ${message}\n\`[ Reason ]\` ${reason}`;
-      channel.send(msg, { files: attachments.map(v => v) });
-    } catch (e) {}
+    let time = require('moment-timezone')().tz('UTC').format('HH:mm:ss');
+    try { time = require('moment-timezone')(config.timezone).tz('UTC').format('HH:mm:ss'); } catch (e) {}
+
+    const msg = client.I18n.translate`\`[${time}]\` \`[${config.moderation.length}]\` ${message}\n\`[ Reason ]\` ${reason}`;
+    channel.send(msg, { files: attachments.map(v => v) });
   },
 };
 
