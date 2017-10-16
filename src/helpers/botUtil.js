e6714c2de85d07edc833df53da8b23c5af761ea6
Kevin B <biojoutk1106@gmail.com>
2017-10-16 19:14:20 +0200

    Updated the prefix string on botUtil

--- src/helpers/botUtil.js
+++ src/helpers/botUtil.js
@@ -1,6 +1,6 @@
 const botUtil = {
   updateGame(client) {
-    client.user.setActivity(`Type ${client.config.prefix}help ! On ${client.guilds.size} servers with ${client.users.size} users.`);
+    client.user.setActivity(`Type ${client.config.discord.prefix}help ! On ${client.guilds.size} servers with ${client.users.size} users.`);
   },
 };
 
