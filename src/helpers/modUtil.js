2d016a2b31ff12e60f22563f2f369bbbb80b2579
Kevin B <biojoutk1106@gmail.com>
2017-10-16 20:05:11 +0200

    Not my problem if people use UTC+ or -

--- src/helpers/modUtil.js
+++ src/helpers/modUtil.js
@@ -1,22 +1,26 @@
 const modUtils = {
   Serverlog(client, guild, message, options) {
-    const config = client.servers.get(guild.id);
-    if (config.switch_serverlog === 0) return;
-    const channel = guild.channels.get(config.channel_serverlog);
-    if (!channel) throw new Error('NO_SERVERLOG_CHANNEL');
+    try {
+      const config = client.servers.get(guild.id);
+      if (config.switch_serverlog === 0) return;
+      const channel = guild.channels.get(config.channel_serverlog);
+      if (!channel) throw new Error('NO_SERVERLOG_CHANNEL');
 
-    const msg = `\`[${require('moment-timezone')().tz(config.timezone).format('HH:mm:ss')}]\` ${message}`;
-    channel.send(msg, options);
+      const msg = `\`[${require('moment-timezone')().tz(config.timezone).format('HH:mm:ss')}]\` ${message}`;
+      channel.send(msg, options);
+    } catch (e) {}
   },
 
   Modlog(client, guild, message, reason, attachments = []) {
-    const config = client.servers.get(guild.id);
-    if (config.switch_modlog === 0) return;
-    const channel = guild.channels.get(config.channel_modlog);
-    if (!channel) throw new Error('NO_MODLOG_CHANNEL');
+    try {
+      const config = client.servers.get(guild.id);
+      if (config.switch_modlog === 0) return;
+      const channel = guild.channels.get(config.channel_modlog);
+      if (!channel) throw new Error('NO_MODLOG_CHANNEL');
 
-    const msg = client.I18n.translate`\`[${require('moment-timezone')().tz(config.timezone).format('HH:mm:ss')}]\` \`[${config.moderation.length}]\` ${message}\n\`[ Reason ]\` ${reason}`;
-    channel.send(msg, { files: attachments.map(v => v) });
+      const msg = client.I18n.translate`\`[${require('moment-timezone')().tz(config.timezone).format('HH:mm:ss')}]\` \`[${config.moderation.length}]\` ${message}\n\`[ Reason ]\` ${reason}`;
+      channel.send(msg, { files: attachments.map(v => v) });
+    } catch (e) {}
   },
 };
 
