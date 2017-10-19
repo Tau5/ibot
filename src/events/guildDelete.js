3565d49452e025b182c5ee12db9cf501ea2ae086
Kevin B <biojoutk1106@gmail.com>
2017-10-19 19:15:11 +0200

    Add checks

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,11 +1,13 @@
 module.exports = async (client, guild) => {
-  /* LOGGING */
-  console.log(`[Servers] Leave - ${guild.name} (ID:${guild.id}) - Check the guilds log for more informations.`);
-  require('fs').appendFile('./logs/guilds.txt', `[${require('moment-timezone')().tz('UTC').format('DD/MM/YYYY HH:mm:ss')}] LEAVE - ${guild.name} (ID:${guild.id}) - Owner: ${guild.owner.user.tag} (ID:${guild.ownerID}) - Members: ${guild.memberCount} (${guild.members.filter(m => m.user.bot).size} bots) - Creation: ${guild.createdAt.toUTCString()}\r\n`, console.error);
+  if (!Object.keys(client.config.blacklist.guilds).includes(guild.id)) {
+    /* LOGGING */
+    console.log(`[Servers] Leave - ${guild.name} (ID:${guild.id}) - Check the guilds log for more informations.`);
+    require('fs').appendFile('./logs/guilds.txt', `[${require('moment-timezone')().tz('UTC').format('DD/MM/YYYY HH:mm:ss')}] LEAVE - ${guild.name} (ID:${guild.id}) - Owner: ${guild.owner.user.tag} (ID:${guild.ownerID}) - Members: ${guild.memberCount} (${guild.members.filter(m => m.user.bot).size} bots) - Creation: ${guild.createdAt.toUTCString()}\r\n`, console.error);
 
-  /* GAME */
-  client.botUtil.updateGame(client);
+    /* GAME */
+    client.botUtil.updateGame(client);
 
-  /* SERVER CONFIGURATION */
-  client.servers.delete(guild.id);
+    /* SERVER CONFIGURATION */
+    client.servers.delete(guild.id);
+  }
 };
