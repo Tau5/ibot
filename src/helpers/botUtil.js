db74c379f44a048c7c9370c1eb9802e5f2270908
iDroid <biojoutk1106@gmail.com>
2017-10-23 22:09:34 +0200

    Guilds updater for discordbots.org

--- src/helpers/botUtil.js
+++ src/helpers/botUtil.js
@@ -2,6 +2,20 @@ const botUtil = {
   updateGame(client) {
     client.user.setActivity(`Type ${client.config.discord.prefix}help ! On ${client.guilds.size} servers with ${client.users.size} users.`);
   },
+
+  updateStatsDiscordBotsOrg(client) {
+    const request = require('request');
+    request.post(`https://discordbots.org/api/bots/${client.user.id}/stats`, {
+      headers: {
+        Authorization: client.config.api.discordbots_org,
+      },
+      form: {
+        server_count: parseInt(client.guilds.size),
+      },
+    }, (err) => {
+      if (err) console.error(err);
+    });
+  },
 };
 
 module.exports = botUtil;
