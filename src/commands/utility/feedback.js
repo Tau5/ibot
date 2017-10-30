e568ceaf557077972f79e0f4eec5ee0c02cde36e
Kevin B <biojoutk1106@gmail.com>
2017-10-30 18:14:49 +0100

    Ah... forgot answer

--- src/commands/utility/feedback.js
+++ src/commands/utility/feedback.js
@@ -4,12 +4,16 @@ exports.execute = async (client, ctx) => {
   if (text) {
     const feedbackChannel = client.channels.get('373425072477569025');
     if (feedbackChannel) {
+      const colors = ['red', 'orange', 'blue', 'green', 'purple', 'yellow', 'brown', 'white', 'grey', 'black'];
       const { MessageEmbed } = require('discord.js');
       const embed = new MessageEmbed()
         .setAuthor(ctx.author.username, ctx.author.displayAvatarURL())
         .setTimestamp(new Date())
-        .setDescription(`Sent by **${ctx.author.username}** in #${ctx.channel.name} (ID:${ctx.channel.id}) on ${ctx.guild.name} (ID:${ctx.guild.id})\n\`\`\`${text}\`\`\``);
+        .setColor(colors[Math.floor(Math.random() * colors.length)])
+        .setDescription(`Sent by **${ctx.author.username}** in **#${ctx.channel.name}** (ID:${ctx.channel.id}) on **${ctx.guild.name}** (ID:${ctx.guild.id})\n\`\`\`${text}\`\`\``);
       feedbackChannel.send({ embed });
+
+      ctx.channel.send('👌');
     }
   }
 };
