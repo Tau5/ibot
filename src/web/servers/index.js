db4f4b804beac66f0c29f5ab37de45d2228a42cf
Kevin B <biojoutk1106@gmail.com>
2017-10-30 21:48:17 +0100

    New design!

--- src/web/servers/index.js
+++ src/web/servers/index.js
@@ -2,7 +2,12 @@ module.exports = (client) => {
   const express = require('express');
   const router = express.Router();
 
-  router.use('/', (req, res) => res.status(200).render('servers', { mutualGuilds: client.guilds.filter(g => g.members.has(req.user.id)), identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') }));
+  router.use('/', (req, res) => {
+    res.status(200).render('servers', {
+      mutualGuilds: client.guilds.filter(g => g.members.has(req.user.id)),
+      identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO'),
+    });
+  });
 
   return router;
 };
