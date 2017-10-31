48bf6ce0fe73c19d81724ac44f4d40dfad53dc7b
Kevin B <biojoutk1106@gmail.com>
2017-10-31 12:15:55 +0100

    Finished logs, since it works :ok_hand:

--- src/web/api/guild/index.js
+++ src/web/api/guild/index.js
@@ -5,7 +5,6 @@ module.exports = (client) => {
   router.post('/updateConfig', (req, res) => {
     if (req.isAuthenticated()) {
       req.body = JSON.parse(decodeURIComponent(Object.keys(req.body)[0]));
-      console.log(require('util').inspect(req.body));
       const guild = client.guilds.get(req.body.guildID);
       if (!guild) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'UNKNOWN_GUILD_ID' });
 
