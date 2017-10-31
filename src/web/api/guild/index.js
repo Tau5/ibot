914000b8a1ee62a2096ef1a1009afaf0d7a916f4
Kevin B <biojoutk1106@gmail.com>
2017-10-31 13:14:24 +0100

    Test-1

--- src/web/api/guild/index.js
+++ src/web/api/guild/index.js
@@ -43,5 +43,52 @@ module.exports = (client) => {
     }
   });
 
+  router.post('/modAction', async (req, res) => {
+    if (req.isAuthenticated()) {
+      req.body = JSON.parse(decodeURIComponent(Object.keys(req.body)[0]));
+      const guild = client.guilds.get(req.body.guildID);
+      if (!guild) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'UNKNOWN_GUILD_ID' });
+      const author = guild.members.get(req.user.id);
+
+      switch (req.body.actionToDo) {
+        case 'ban':
+          if (!author.hasPermission('BAN_MEMBERS')) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'You do not have the permission to ban a member!' });
+          break;
+        case 'kick':
+          if (!author.hasPermission('KICK_MEMBERS')) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'You do not have the permission to kick a member!' });
+          break;
+        case 'warn':
+          if (!author.hasPermission('KICK_MEMBERS')) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'You do not have the permission to warn a member!' });
+          break;
+        case 'unwarn':
+          if (!author.hasPermission('KICK_MEMBERS')) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'You do not have the permission to unwarn a member!' });
+          break;
+        default:
+          break;
+      }
+
+      let member = client.findersUtil.findMember(guild, req.body.user);
+      if (member.size === 0) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'The specified user was not found!' });
+      else if (member.size > 1) return res.header('Access-Control-Allow-Origin', '*').status(696).json({ message: client.findersUtil.formatMembers(client, member) });
+      else member = member.first();
+
+      if (req.body.actionToDo === 'ban') {
+        member.ban(req.body.reason).then(() => {
+          client.modUtil.Modlog(client, guild, client.I18n.translate`🔨 **${author.tag}** banned **${member.user.tag}** (ID:${member.id}).`, req.body.reason);
+          return res.header('Access-Control-Allow-Origin', '*').status(500).json({ message: `🔨 **${author.user.tag}** has been banned for ${req.body.reason} successfully!` });
+        }).catch(e => res.header('Access-Control-Allow-Origin', '*').status(500).json({ message: e }));
+      } else if (req.body.actionToDo === 'kick') {
+        member.kick(req.body.reason).then(() => {
+          client.modUtil.Modlog(client, guild, client.I18n.translate`👢 **${author.tag}** kicked **${member.user.tag}** (ID:${member.id}).`, req.body.reason);
+          return res.header('Access-Control-Allow-Origin', '*').status(500).json({ message: `👢 **${member.user.tag}** has been kicked for ${req.body.reason} successfully!` });
+        }).catch(e => res.header('Access-Control-Allow-Origin', '*').status(500).json({ message: e }));
+      }
+    } else {
+      res.header('Access-Control-Allow-Origin', '*').status(401).json({
+        message: 'NOT_LOGGED_IN',
+      });
+    }
+  });
+
   return router;
 };
--- src/web/server/index.js
+++ src/web/server/index.js
@@ -3,33 +3,6 @@ module.exports = (client) => {
   const router = express.Router();
   const timezones = require('moment-timezone').tz.names();
 
-  router.post('/update/:id', (req, res) => {
-    if (!client.guilds.has(req.params.id)) return res.status(404).render('error', { code: '404', identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') });
-    const config = client.servers.get(req.params.id);
-    const newConfig = {
-      channel_welcome: req.body.channel_welcome,
-      channel_serverlog: req.body.channel_serverlog,
-      channel_modlog: req.body.channel_modlog,
-      message_welcome: req.body.message_welcome,
-      message_leaving: req.body.message_leaving,
-      switch_welcome: ((req.body.channel_welcome !== 'NOT_SET' && req.body.message_welcome !== 'NOT_SET') ? parseInt(req.body.switch_welcome) : 0),
-      switch_leaving: ((req.body.channel_welcome !== 'NOT_SET' && req.body.message_leaving !== 'NOT_SET') ? parseInt(req.body.switch_leaving) : 0),
-      switch_serverlog: (req.body.channel_serverlog !== 'NOT_SET' ? parseInt(req.body.switch_serverlog) : 0),
-      switch_modlog: (req.body.channel_modlog !== 'NOT_SET' ? parseInt(req.body.switch_modlog) : 0),
-      switch_clearbackup: parseInt(req.body.switch_clearbackup),
-      roleme: config.roleme,
-      custom_prefixes: config.custom_prefixes,
-      moderation: config.moderation,
-      imported_tags: config.imported_tags,
-      ignored_channels: config.ignored_channels,
-      timezone: req.body.timezone,
-      locale: req.body.locale,
-    };
-
-    client.servers.set(req.params.id, newConfig);
-    res.redirect(`/server/${req.params.id}`);
-  });
-
   router.use('/:id', (req, res) => {
     if (!client.guilds.has(req.params.id)) return res.status(404).render('error', { code: '404', identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') });
     const guild = client.guilds.get(req.params.id);
