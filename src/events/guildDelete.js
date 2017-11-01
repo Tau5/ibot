41a1dfffe4ed95ceafc0ff953f5aebc82b0db108
Kevin B <biojoutk1106@gmail.com>
2017-11-01 10:02:14 +0100

    Forgot to update the server count @ Dbots.org when a guild was removed

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -4,8 +4,9 @@ module.exports = async (client, guild) => {
     console.log(`[Servers] Leave - ${guild.name} (ID:${guild.id}) - Check the guilds log for more informations.`);
     require('fs').appendFile('./logs/guilds.txt', `[${require('moment-timezone')().tz('UTC').format('DD/MM/YYYY HH:mm:ss')}] LEAVE - ${guild.name} (ID:${guild.id}) - Owner: ${guild.owner.user.tag} (ID:${guild.ownerID}) - Members: ${guild.memberCount} (${guild.members.filter(m => m.user.bot).size} bots) - Creation: ${guild.createdAt.toUTCString()}\r\n`, console.error);
 
-    /* GAME */
+    /* GAME & DISCORD BOTS STATS */
     client.botUtil.updateGame(client);
+    client.botUtil.updateStatsDiscordBotsOrg(client);
 
     /* SERVER CONFIGURATION */
     client.servers.delete(guild.id);
