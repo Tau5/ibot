5385652f22c8dfd861c7ea0b684a9c1bf430ef72
Kevin B <biojoutk1106@gmail.com>
2017-11-01 21:33:43 +0100

    Added telephone number to i:help

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,5 +1,5 @@
 exports.execute = async (client, ctx) => {
-  ctx.channel.send('🔗 https://github.com/iDroid27210/ibot/wiki/Commands');
+  ctx.channel.send('🔗 https://github.com/iDroid27210/ibot/wiki/Commands\n☎ `i:call 1-000-000`');
 };
 
 exports.conf = {
