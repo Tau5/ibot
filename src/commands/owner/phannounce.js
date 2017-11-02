469f2e37888d8bdd8059d32f9b1fd0e1a16f89e2
Kevin B <biojoutk1106@gmail.com>
2017-11-02 12:20:46 +0100

    Announcements system using phones

---
+++ src/commands/owner/phannounce.js
@@ -0,0 +1,28 @@
+exports.execute = async (client, ctx) => {
+  const text = ctx.args.join(' ');
+  if (!text) return ctx.channel.send('❌ The announcement cannot be empty.');
+  if (text.length > 1024) return ctx.channel.send('❌ The announcement length cannot exceed 1024 characters.');
+
+  client.numbers.filter(g => g !== '302208427797643266').forEach((guildID) => {
+    const guild = client.guilds.get(guildID);
+    const config = client.servers.get(guildID);
+    const phoneChannel = guild.channels.get(config.channel_phone);
+    if (phoneChannel) {
+      const message = [
+        '📡 This is an automatic announcement sent from the support (`1-000-000`).',
+        `${text}`,
+        'ℹ The support is available at 1-000-000. Use it only when needed (help using iBot, reporting bugs, etc).',
+      ];
+
+      ctx.channel.send(message.join('\n'));
+    }
+  });
+
+  ctx.channel.send('👌 Announcement sent!');
+};
+
+exports.conf = {
+  name: 'phannounce',
+  aliases: ['announcement'],
+  public: false,
+};
