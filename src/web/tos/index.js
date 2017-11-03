ca9a732847590fdaf6fc5dd009ba9a4faed1a0c8
Kevin B <biojoutk1106@gmail.com>
2017-11-03 15:22:42 +0100

    ESLint fixes

--- src/web/tos/index.js
+++ src/web/tos/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).render('tos', {
-    identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO')
+    identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO'),
   });
 });
 
