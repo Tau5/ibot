6cd7f92897bc5582fbecd96da9833e807476585d
Kevin B <biojoutk1106@gmail.com>
2017-11-03 18:25:33 +0100

    Presence update rework

--- src/web/api/bot/index.js
+++ src/web/api/bot/index.js
@@ -14,15 +14,22 @@ module.exports = (client) => {
     });
   });
 
-  router.use('/leaveGuild/:id', (req, res) => {
+  router.post('/changePresence', (req, res) => {
     if (req.isAuthenticated()) {
       if (req.session.user.id !== '205427654042583040') return res.header('Access-Control-Allow-Origin', '*').status(403).json({ message: 'UNAUTHORIZED' });
+      req.body = JSON.parse(decodeURIComponent(Object.keys(req.body)[0]));
 
-      const guild = client.guilds.get(req.body.guildID);
-      if (!guild) return res.header('Access-Control-Allow-Origin', '*').status(404).json({ message: 'UNKNOWN_GUILD_ID' });
-      guild.leave().then(() => {
-        res.header('Access-Control-Allow-Origin', '*').status(200).json({ message: 'SUCCESS' });
-      }).catch(e => res.header('Access-Control-Allow-Origin', '*').status(500).json({ message: `ERROR: ${e}` }));
+      client.user.setPresence({
+        status: req.body.status,
+        activity: {
+          type: 0,
+          name: req.body.game,
+        },
+      }).then((newPresence) => {
+        res.header('Access-Control-Allow-Origin', '*').status(200).json(newPresence);
+      }).catch(() => {
+        res.header('Access-Control-Allow-Origin', '*').status(500);
+      });
     } else {
       res.header('Access-Control-Allow-Origin', '*').status(401).json({
         message: 'NOT_LOGGED_IN',
