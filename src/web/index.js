91f319fbd0797ee91f87284342db8c0ba70bb12d
Kevin B <biojoutk1106@gmail.com>
2017-11-04 11:46:08 +0100

    Remove updateSession where we do not need it

--- src/web/index.js
+++ src/web/index.js
@@ -79,8 +79,10 @@ module.exports = (client) => {
     .set('views', `${__dirname}/templates/`);
 
   // Page handling
-  app.get('/', updateSession, (req, res) => {
-    res.status(200).render('index', { identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') });
+  app.get('/', (req, res) => {
+    res.status(200).render('index', {
+      identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO'),
+    });
   });
 
   app
@@ -91,7 +93,7 @@ module.exports = (client) => {
     .use('/user', checkAuth, updateSession, require('./user')(client))
     .use('/server', checkAuth, updateSession, require('./server')(client))
     .use('/invite', checkAuth, updateSession, require('./invite')(client))
-    .use('/tos', updateSession, require('./tos'))
+    .use('/tos', require('./tos'))
     .use('*', (req, res) => res.status(404).render('error', { code: '404', identity: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : 'NO') }));
 
   client.app = https.createServer({
